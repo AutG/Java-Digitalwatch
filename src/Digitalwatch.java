d21fc4b9f9adde1591854f1c692d6170bb234eb6
Patrick <p.kucera@me.com>
2018-02-02 18:24:04 +0100

    Done

---
+++ src/Digitalwatch.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Date;
+
+public class Digitalwatch extends JPanel implements Runnable {
+
+    private JLabel label;
+    private Date date;
+    private Font font = new Font("Verdana", Font.PLAIN, 20);
+    private Thread thread;
+
+
+
+    public Digitalwatch(){
+        label = new JLabel();
+        label.setFont(font);
+        this.add(label);
+        start();
+    }
+
+
+    private void getDate(){
+        label.setText(date.toString());
+    }
+
+    public void start(){
+        if (thread == null){
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
+
+
+    public void run(){
+        while (true){
+            date = new Date();
+            getDate();
+            try{
+                Thread.sleep(1000);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void main(String[]args){
+        Digitalwatch c = new Digitalwatch();
+        JFrame f = new JFrame();
+        f.add(c);
+        f.setSize(350,80);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+}
